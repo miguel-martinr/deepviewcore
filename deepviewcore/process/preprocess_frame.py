2c3779d769375ce44841d6105a130698390adee6
Miguel Martín <alu0101209777@ull.edu.es>
2022-06-24 19:31:13 +0100

    print removed

--- deepviewcore/process/preprocess_frame.py
+++ deepviewcore/process/preprocess_frame.py
@@ -3,7 +3,6 @@ from deepviewcore.filters import preprocess
 
 def preprocess_frame(frame, options):
     preprocessed = frame
-    print(options)
     for meta in preprocess:
         filter = meta["filter"]
         
