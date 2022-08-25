822bab88215ebbacda02da9ca57d24a6aece55fb
Miguel Martín <alu0101209777@ull.edu.es>
2022-08-25 01:26:17 +0100

    version updated

--- deepviewcore/tests/test.py
+++ deepviewcore/tests/test.py
@@ -1,26 +1,23 @@
 import cv2 as cv
 
-from process.detect_objects import detect_objects_in_frame, draw_contours
+from deepviewcore.process.detect_objects import detect_objects_in_frame, draw_contours
 
 
-cap = cv.VideoCapture("C:/Users/migue/Documents/U/4to/TFG/DeepView/static/videos/Pez1.mp4")
+cap = cv.VideoCapture("C:/Users/migue/Documents/U/4to/TFG/DeepView/static/videos/plancton_1.mp4")
 
 # Set frame index
-cap.set(cv.CAP_PROP_POS_FRAMES, 55)
+cap.set(cv.CAP_PROP_POS_FRAMES, 33)
+window = cv.namedWindow("Frame", cv.WINDOW_NORMAL)
 
 while cap.isOpened():
     ret, frame = cap.read()
-    if not ret:
-        break
-    [objects, frameHasEvent] = detect_objects_in_frame(frame, {"events": {"minArea": 200, "active": True}})
-
-    draw_contours(frame, objects)
-    if frameHasEvent:
-        print("EVENT")
-    else:
-        print("NO EVENT")
     
-    cv.imshow("frame", frame)
+    contours = detect_objects_in_frame(frame, {})
+    draw_contours(frame, contours)
+
+    cv.imshow("Frame", frame)
+
+    
     if cv.waitKey(0) & 0xFF == ord('q'):
         break
 
